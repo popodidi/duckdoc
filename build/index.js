182e75836be84952fc3f328f801b916320240567
Hao <changhao@sendbest.cc>
2017-04-07 13:19:47 +0800

    testing

--- build/index.js
+++ build/index.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = duck;
+
+var _Duck = require('./lib/Duck');
+
+var _Duck2 = _interopRequireDefault(_Duck);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function duck(projectName, dir, outputPath) {
+  return new _Duck2.default(projectName, dir, outputPath);
+}
